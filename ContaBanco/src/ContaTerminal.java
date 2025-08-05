0f3cb4395763bb8bc6210c5dd942d4dff3359c19
Akanni Silva <akanni029@gmail.com>
2025-08-05 17:00:54 -0300

    adicinando TODO

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,8 @@
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Todo: conhecer e importar a classe Scanner
+        // exibir mensagem para o usuário
+        // obter pelo scanner os dados digitados no terminal
+        // exibir mensagem de conta criada com sucesso
     }
 }
