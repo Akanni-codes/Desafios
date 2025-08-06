4c50585a2ba592e986ee2196fb01abaa45dac75b
Akanni Silva <akanni029@gmail.com>
2025-08-06 10:57:41 -0300

    Criando e manipulando variaveis

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,8 +1,25 @@
+import java.util.Scanner;
+// importar a classe Scanner
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        // Todo: conhecer e importar a classe Scanner
+       public static void main(String[] args) throws Exception {
+        int numeroConta;
+        int agencia;
+        String nomeCliente;
+        double saldo;
+        
+        System.out.print("Bem vindo ao Banco AkCode, por favor digite o seu nome !");
         // exibir mensagem para o usuário
+        nomeCliente = new Scanner(System.in).nextLine();
         // obter pelo scanner os dados digitados no terminal
+        System.out.println("Por favor, digite o número da Agência !");
+        agencia = new Scanner(System.in).nextInt();
+        System.out.println("Número da Conta (sem dígito verificador)");
+        numeroConta = new Scanner(System.in).nextInt();
+        System.out.println("Saldo da conta");
+        saldo = new Scanner(System.in).nextDouble();
+        
+        
+        System.out.println("Olá " + nomeCliente + " ,obrigado por criar uma conta em nosso banco, sua agência é " + agencia + " , conta " + numeroConta + " e seu saldo " + saldo +" já está disponível para saque");
         // exibir mensagem de conta criada com sucesso
     }
 }
